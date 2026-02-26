eb12c110143a4e3dd00e5231cb4088e2185013d8
otaviouu <otavio.venancio.oliveira@usp.br>
2026-02-26 13:45:48 -0300

    🔥 initial commit 🔥

---
+++ lib/busca_estagio/mailer.ex
@@ -0,0 +1,3 @@
+defmodule BuscaEstagio.Mailer do
+  use Swoosh.Mailer, otp_app: :busca_estagio
+end
---
+++ lib/busca_estagio_web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule BuscaEstagioWeb.PageController do
+  use BuscaEstagioWeb, :controller
+
+  def home(conn, _params) do
+    render(conn, :home)
+  end
+end
