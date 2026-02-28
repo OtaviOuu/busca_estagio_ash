0112f8df05d78859a67597557db298ef8078c10d
otaviouu <otavio.venancio.oliveira@usp.br>
2026-02-28 11:51:17 -0300

    add logo + improve some ui stuff

--- lib/busca_estagio_web/components/layouts.ex
+++ lib/busca_estagio_web/components/layouts.ex
@@ -65,25 +65,12 @@ defmodule BuscaEstagioWeb.Layouts do
         </div>
       </div>
       <div class="navbar-center">
-        <.link navigate={~p"/"} class="btn btn-ghost text-xl">daisyUI</.link>
+        <.link navigate={~p"/"} class="btn btn-ghost normal-case text-xl">
+          <img src={~p"/images/logo.png"} alt="BuscaEstágio" class="h-12 w-auto object-contain" />
+        </.link>
       </div>
       <div class="navbar-end">
-        <button class="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
+
         <button class="btn btn-ghost btn-circle">
           <div class="indicator">
             <svg
