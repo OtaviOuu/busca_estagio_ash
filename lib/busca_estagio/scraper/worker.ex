ca55ed22aa0688de6ca58d64635d97b6467a18bd
otaviouu <otavio.venancio.oliveira@usp.br>
2026-02-28 23:23:00 -0300

    aaaaaa

--- lib/busca_estagio/scraper/worker.ex
+++ lib/busca_estagio/scraper/worker.ex
@@ -9,7 +9,7 @@ defmodule BuscaEstagio.Scraper.Worker do
     with {:ok, ufmg_icex_attrs} <- BuscaEstagio.Scraper.scrape_ufmg_icex_internships(),
          {:ok, usp_eesc_attrs} <- BuscaEstagio.Scraper.scrape_usp_eesc_internships() do
       all_attrs =
-        (usp_eesc_attrs ++ ufmg_icex_attrs ++ usp_fearp_attrs) |> Enum.uniq_by(& &1.url)
+        (usp_eesc_attrs ++ ufmg_icex_attrs) |> Enum.uniq_by(& &1.url)
 
       Ash.bulk_create(all_attrs, BuscaEstagio.Internships.Internship, :create,
         upsert?: true,
